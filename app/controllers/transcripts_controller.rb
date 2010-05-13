f669dbe945e5e3f95866b681235e9469b2cfcbc1
Wayne <wayne.graham@gmail.com>
2010-05-13 15:03:03 -0400

    Added override to ActiveRecord find

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -3,15 +3,14 @@ class TranscriptsController < ApplicationController
   XSLT_PATH = File.join(RAILS_ROOT, 'db', 'tei', 'faulkner.xsl')
   
   def index  
+    #TODO: figure out what to do with the browse
     @files = Dir[RAILS_ROOT + '/db/tei/*.xml']
   end
   
   def show
-    xml_path = File.join(RAILS_ROOT, 'db', 'tei', 'wfaudio01_1.xml')
-
-    @xml = Nokogiri::XML(File.read(xml_path))
-    @xslt = Nokogiri::XSLT(File.read(XSLT_PATH))
-
-    @transformed = @xslt.transform(@xml)
+    
+    # check if the file exists
+    @transformed = Transcript.find(params[:file])
+    
   end
 end
---
+++ app/models/transcript.rb
@@ -0,0 +1,13 @@
+class Transcript < ActiveRecord::Base
+  
+  # We're treating the file system as the "datastore" 
+  # Find overrides and returns a reference to the generated partial
+  def self.find(*args) 
+    # get the file
+    fname = File.basename(args.to_s)
+      
+    return output_name = RAILS_ROOT + "/public/tei/_" + fname.gsub('.xml', '.html.erb')
+    
+  end
+  
+end
