0de06c560f841c0c5148920d4de0992ce8e3d3dc
Joseph Gilbert <jfg9x@d-137-155-196.bootp.virginia.edu>
2010-05-14 10:50:30 -0400

    sidebar and js css improvements

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+	// js-only styles
+	$('dl').css('cursor','pointer');
+	$('dl a').css('text-decoration','none');
+	$('dl a').css('font-weight','normal');
+	
+	$('dl:odd').css('background-color','#f5f5f5');
+	$('dl:even').css('background-color','#eed');
+	
+	$('dl').hover(function() {
+		$(this).css('background-color','#e4ebfd');
+	}, function() {
+		if($(this).is('dl:nth-child(odd)')) {
+			$(this).css('background-color','#f5f5f5');
+		} else {
+			$(this).css('background-color','#eed');
+		}
+	});
+	
+	$('dl').click(function() {
+		window.location = $(this).find('a').attr('href');
+	});
+});
