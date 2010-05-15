0de8f9e6b7b0d3bec03c9a29d64429d3ba4cc8a6
Wayne <wayne.graham@gmail.com>
2010-05-15 09:05:23 -0400

    added rspec; removed unused Pages controller and added a slugignore file for heroku deployments

---
+++ spec/controllers/transcripts_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe TranscriptsController do
+
+  #Delete this example and add some real ones
+  it "should use TranscriptsController" do
+    controller.should be_an_instance_of(TranscriptsController)
+  end
+
+end
---
+++ spec/models/transcripts_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Transcripts do
+  before(:each) do
+    @valid_attributes = {
+      
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Transcripts.create!(@valid_attributes)
+  end
+end
