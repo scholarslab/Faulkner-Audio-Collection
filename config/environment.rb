2a73278b79570f2e5807b4f61feaa65a08a79791
Wayne <wayne.graham@gmail.com>
2010-05-29 14:35:14 -0400

    updated

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
