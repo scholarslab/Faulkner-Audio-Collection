c78c1470a6a25f8e8d2b5557937ee2ea0837709e
Wayne Graham <wayne.graham@virginia.edu>
2011-07-12 13:53:03 -0400

    Updated gem dependencies and rails version to latest 2.3.12

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.12' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -16,8 +16,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "nokogiri"
-  config.gem "rsolr"
-  config.gem "rsolr-ext"
+  config.gem "rsolr", :version => '0.12.1'
+  config.gem "rsolr-ext", :version => '0.12.1'
   config.gem "will_paginate"
 
   # Skip frameworks you're not going to use. To use Rails without a database,
